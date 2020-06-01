33828ef598ce564f61aff6e793a7af2db28bd98a
Huynhzip3 <huynhlx62@wru.vn>
2020-06-01 22:06:19 +0700

    finsh-project

--- application/controllers/lienhe.php
+++ application/controllers/lienhe.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class lienhe extends CI_Controller {
 	public function index()
 	{
-        $this->load->view('layout/index-lienhe');
+        $this->load->view('lienhe/index-lienhe');
 	}
 }
--- application/controllers/tuvan.php
+++ application/controllers/tuvan.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class tuvan extends CI_Controller {
 	public function index()
 	{
-        $this->load->view('layout/index-tuvan');
+        $this->load->view('tuvan/index-tuvan');
 	}
 }
